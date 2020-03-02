594fc9b4c05f4991a17f70b7ef192782e8bd4264
Eric van der Toorn <erictoorn@gmail.com>
2020-03-02 23:00:34 +0100

    initial

---
+++ index.js
@@ -0,0 +1,44 @@
+const ipc = require('node-ipc')
+const { exec } = require('child_process');
+ipc.config.id = "world";
+ipc.config.retry = 1500;
+
+
+ipc.serve(
+    function () {
+        ipc.server.on(
+            'message',
+            function (data, socket) {
+                ipc.log('got a message: ', data)
+                console.log("got a message")
+            }
+        );
+        ipc.server.on('update', function (data, socket) {
+            console.log('update triggered')
+            update(data);
+        })
+        ipc.server.on('socket.disconnected',
+            function (socket, destroyedSocketID) {
+                ipc.log("client " + destroyedSocketID + " has disconnected")
+            }
+        )
+    }
+
+)
+
+function update(path) {
+    exec("cd \'" + path + "\' && sleep 1000 git pull", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+    
+}
+
+ipc.server.start();
